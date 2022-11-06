8ed6ee66dd6c0971013c619da2ac9ae91bca449b
Paweł <Pawel2001@vp.pl>
2022-11-06 13:53:48 +0100

    wer.1.4

--- main.cpp
+++ main.cpp
@@ -5,14 +5,29 @@ using namespace std;
 int main()
 {
 	
+	double Ax, By, C;
+	
+	cout << "Podaj Ax" << endl;
+	cin >> Ax;
+	
+	cout << "Podaj By" << endl;
+	cin >> By;
+
+	cout << "Podaj C" << endl;
+	cin >> C;
+
+
+	By = -By;
+
+	Ax = Ax/By;
+	C = C/By;
+
+	By = By/By;
+
 	double a, b;
-	
-	cout << "Podaj a" << endl;
-	cin >> a;
-	
-	cout << "Podaj b" << endl;
-	cin >> b;
-	
+
+	a = Ax;
+	b = C;
 	
 	
 	if(a == 0 && b != 0)
@@ -21,7 +36,7 @@ int main()
 	}
 	else if(a == 0 && b == 0)
 	{
-		cout << "Funkcja ma nieskoñczenie miejsc zerowych" << endl;	
+		cout << "Funkcja ma nieskonczenie miejsc zerowych" << endl;	
 	}
 	else if(a != 0)
 	{
